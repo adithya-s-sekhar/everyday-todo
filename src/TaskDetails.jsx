fb0c634132ed36f8c20a07ef2f92d13a286db4d9
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-07 13:53:25 +0530

    simplify: remove won't do

--- src/TaskDetails.jsx
+++ src/TaskDetails.jsx
@@ -7,7 +7,7 @@ const TaskDetails = () => {
     const navigate = useNavigate();
 
     const doneTask = () => {
-        const newTask = { ...task, status:1 };
+        const newTask = { ...task, completed: true };
         fetch(process.env.REACT_APP_JSON_URL+'/tasks/'+task.id,{
             method: 'DELETE'
         }).then(() => {
@@ -20,21 +20,7 @@ const TaskDetails = () => {
     }
 
     const notDoneTask = () => {
-        const newTask = { ...task, status:0 };
-        fetch(process.env.REACT_APP_JSON_URL+'/tasks/'+task.id,{
-            method: 'DELETE'
-        }).then(() => {
-            fetch(process.env.REACT_APP_JSON_URL+'/tasks',{
-                method: 'POST',
-                headers: { 'Content-Type':'application/json' },
-                body: JSON.stringify(newTask)
-            })
-        })
-    }
-
-    const wontDoTask = () => {
-        const newTask = { ...task, status:2 };
-        console.log(newTask);
+        const newTask = { ...task, completed: false };
         fetch(process.env.REACT_APP_JSON_URL+'/tasks/'+task.id,{
             method: 'DELETE'
         }).then(() => {
@@ -61,19 +47,19 @@ const TaskDetails = () => {
             { task &&
                 <div className = "task-details">
                     <div className = "task-details-title">
-                    <h1>{ task.title }</h1>
+                        <div className="task-details-title-text">
+                            <h1>{ task.title }</h1>
+                        </div>
+                        <div className="task-details-title-status">
+                            { !task.completed && <p className = "status-text st-not-done">Status: Not done</p> }
+                            { task.completed && <p className = "status-text st-done">Status: Done</p> }
+                        </div>
                     </div>
                     <div className = "task-details-actions">
-                        <div className = "task-details-status">
-                        { (task.status === 0) && <p className = "status-text st-not-done">Status: Not done</p> }
-                        { (task.status === 1) && <p className = "status-text st-done">Status: Done</p> }
-                        { (task.status === 2) && <p className = "status-text st-wont-do">Status: Won't Do</p> }
-                        </div>
                         <div className = "task-buttons">
-                                { (task.status === 0) && <button className = "task-button-done" onClick = { doneTask }>Mark as Done</button> }
-                                { (task.status === 1) && <button className = "task-button-not-done" onClick = { notDoneTask }>Mark as not done</button> }
-                                { (task.status === 0) && <button className = "task-button-wont-do" onClick = { wontDoTask }>Won't Do</button> }
-                                { (task.status === 1 || task.status === 2) && <button className = "task-button-delete" onClick = { deleteTask }>Delete</button> }
+                            { !task.completed && <button className = "task-button-done" onClick = { doneTask }>Mark as Done</button> }
+                            { task.completed && <button className = "task-button-not-done" onClick = { notDoneTask }>Mark as not done</button> }
+                            <button className = "task-button-delete" onClick = { deleteTask }>Delete</button>
                         </div>
                     </div>
                     <div className="task-details-details">
--- src/TaskItem.jsx
+++ src/TaskItem.jsx
@@ -12,9 +12,8 @@ const TaskItem = (props) => {
                                 <h2>{ task.title }</h2>
                             </div>
                             <div className = "task-item-status">
-                                { (task.status === 0) && <p className = "status-text st-not-done">Status: Not done</p> }
-                                { (task.status === 1) && <p className = "status-text st-done">Status: Done</p> }
-                                { (task.status === 2) && <p className = "status-text st-wont-do">Status: Won't Do</p> }
+                                { !task.completed && <p className = "status-text st-not-done">Status: Not done</p> }
+                                { task.completed && <p className = "status-text st-done">Status: Done</p> }
                             </div>
                             <div className = "task-item-details">
                                 <h3>Details</h3>
