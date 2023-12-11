55bffe9efe13772d739076ef4051d1f7a1496872
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-11 09:27:10 +0530

    Add task count

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,28 @@
 import TaskList from "./TaskList";
 import useFetch from "../hooks/useFetch";
+import { useEffect, useState } from "react";
 
 const Home = () => {
     const { data: tasks, dbLoaded, error } = useFetch(process.env.REACT_APP_JSON_URL);
 
+    const [taskCount,setTaskCount] = useState('');
+
+    let count = 0;
+
+    useEffect(() => {
+        if (tasks) {
+            for (let i of tasks) {
+                if (!i.completed) {
+                    count++;
+                }
+            }
+        }
+        setTaskCount(count);
+    },[tasks,taskCount]);
+
     return(
         <div className = "home">
-            <h1>Tasks</h1>
+            <h1>Tasks ({taskCount} left)</h1>
             { !dbLoaded && <div>Loading..</div> }
             { error && <div>Error: { error }</div> }
             { tasks && <TaskList tasks = { tasks }/> }
@@ -14,4 +30,4 @@ const Home = () => {
     );
 }
 
-export default Home
+export default Home;
